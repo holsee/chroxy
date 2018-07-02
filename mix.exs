dc2ce030cb5aaa268d60fe5c744321ea647df8f3
Steven Holdsworth <sholdsworth@gmail.com>
2018-07-02 18:45:14 +0100

    bumped to 0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chroxy.MixProject do
   def project do
     [
       app: :chroxy,
-      version: "0.3.2",
+      version: "0.4.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
