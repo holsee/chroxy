13b8d3021e02389d5f63bb4d9862f60c05eb59f9
Steven Holdsworth <sholdsworth@gmail.com>
2020-01-06 15:26:00 +0000

    Bumped version to 0.6.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Chroxy.MixProject do
   def project do
     [
       app: :chroxy,
-      version: "0.6.2",
+      version: "0.6.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
